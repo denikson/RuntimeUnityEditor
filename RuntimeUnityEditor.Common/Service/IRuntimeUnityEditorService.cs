6975e6d290435bd8412d8a157bacda9c33dc74f6
ghorsington <neigh@coder.horse>
2019-09-09 21:59:02 +0300

    Set up DllExport and deps for UI

---
+++ RuntimeUnityEditor.Common/Service/IRuntimeUnityEditorService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MiniIPC.Attributes;
+
+namespace RuntimeUnityEditor.Common.Service
+{
+    [MiniService]
+    public interface IRuntimeUnityEditorService
+    {
+        void Echo(string message);
+    }
+}
---
+++ RuntimeUnityEditor.UI/Export.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RuntimeUnityEditor.UI
+{
+    public static class Export
+    {
+        private static bool guiInitialized = false;
+        private static Thread guiThread;
+
+        [DllExport(CallingConvention.StdCall)]
+        public static void ShowGUI()
+        {
+            if (!guiInitialized)
+                StartGUI();
+            else
+                GUIMain.form.Invoke((Action)(() => { GUIMain.form.Visible = true; }));
+        }
+
+        private static void StartGUI()
+        {
+            guiThread = new Thread(GUIMain.Run);
+            guiThread.Start();
+            guiInitialized = true;
+        }
+    }
+
+    internal static class GUIMain
+    {
+        public static Form1 form;
+
+        [STAThread]
+        public static void Run()
+        {
+            Application.ThreadException += ApplicationOnThreadException;
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomainOnUnhandledException;
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            form = new Form1();
+            Application.Run(form);
+        }
+
+        private static void CurrentDomainOnUnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.ExceptionObject.ToString(), "Unhandled domain exception!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private static void ApplicationOnThreadException(object sender, ThreadExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.ToString(), "Unhandled application exception!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
